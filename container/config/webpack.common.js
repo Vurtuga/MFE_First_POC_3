c9a954c0a587c517a6971c61518bb77bef32b628
Eng_Mohamed Khalifa <Eng_m.khalifa@outlook.com>
2023-05-03 15:26:45 +0300

    base container

--- container/config/webpack.common.js
+++ container/config/webpack.common.js
@@ -1,24 +1,41 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
 module.exports = {
     module:{
         rules: [
+            {
+                test: /\.(png|jpe?g|gif|woff|svg|eot|ttf)$/i,
+                use: [
+                    { loader: 'file-loader'}
+                ]
+            },
+            {
+                test: /\.vue$/,
+                use: 'vue-loader'
+            },
+            {
+                test: /\.scss|\.css$/,
+                use: ['style-loader','css-loader']
+            },
             {
                 test: /\.m?js$/,
                 exclude: /node_modules/,
                 use:{
                     loader:'babel-loader',
                     options:{
-                        presets: ['@babel/preset-react','@babel/preset-env'],
+                        presets: ['@vue/cli-plugin-babel/preset','@babel/preset-env'],
                         plugins: ['@babel/plugin-transform-runtime'],
                     }
                 }
             }
-        ],
+        ]
     },
     plugins:[
         new HtmlWebpackPlugin({
             template: './public/index.html',
+            title: 'Main Vue2 Container',
+            templateParameters:{
+                BASE_URL: `http://localhost:8080/`
+            }
         })
     ]
 }
--- container/config/webpack.dev.js
+++ container/config/webpack.dev.js
@@ -1,27 +1,30 @@
+const path = require("path");
 const {merge} = require('webpack-merge');
 const commonConfig = require('./webpack.common');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const packageJson = require( '../package.json');
+const { VueLoaderPlugin } = require('vue-loader');
 
 const devConfig = {
     mode: 'development',
+    entry: path.resolve(__dirname, "../src/main.js"),
     output:{
-        publicPath: 'http://localhost:8080/',
+        publicPath: `http://localhost:8080/`,
     },
     devServer:{
         port: 8080,
         historyApiFallback: true,
+        headers:{
+            'Access-Control-Allow-Origin': '*'
+        }
     },
     plugins:[
         new ModuleFederationPlugin({
             name: 'container',
-            remotes :{
-                marketing: 'marketing@http://localhost:8081/remoteEntry.js',
-                auth: 'auth@http://localhost:8082/remoteEntry.js',
-                dashboard: 'dashboard@http://localhost:8083/remoteEntry.js',
-            },
+
             shared: packageJson.dependencies ,
-        }) 
+        }),
+        new VueLoaderPlugin()
     ]
 }
 
