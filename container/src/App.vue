5e252e673f6c1a0853c0d8acef02954289fca078
Eng_Mohamed Khalifa <Eng_m.khalifa@outlook.com>
2023-05-11 14:22:16 +0300

    only first routing bug happened

--- container/src/App.vue
+++ container/src/App.vue
@@ -2,7 +2,7 @@
   <div id="_container-wrapper">
     <router-link to="/" key="go_home" exact-path>Go to Home</router-link>
     <router-link to="/dashboard" key="go_dashboard">Go to dashboard</router-link>
-    <router-view  :key="$route.fullPath"></router-view>
+    <router-view></router-view>
   </div>
 </template>
 
